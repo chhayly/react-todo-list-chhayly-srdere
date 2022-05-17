5cc812318007f4a350b3a966e41062e0e08771c1
chhayly sreng <chhaylysreng@Chhaylys-Macbook-Air.local>
2022-05-17 10:30:59 +0700

    add env example

--- src/DataAccessLayers/firebase.ts
+++ src/DataAccessLayers/firebase.ts
@@ -1,23 +1,22 @@
 // Initialize Cloud Firestore through Firebase
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getDatabase } from "firebase/database"
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 initializeApp({
-  apiKey: "AIzaSyDOzqfnUAGVC42n0MoKvSDbceJiI1Ni0jA",
+  apiKey: process.env.apiKey,
   authDomain: "todo-v2-5b628.firebaseapp.com",
-  databaseURL: "https://todo-v2-5b628-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://todo-v2-5b628-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "todo-v2-5b628",
   storageBucket: "todo-v2-5b628.appspot.com",
   messagingSenderId: "1043867628995",
   appId: "1:1043867628995:web:aec184070af313fabc7976",
-  measurementId: "G-LC5BPFLJBL"
-  });
+  measurementId: "G-LC5BPFLJBL",
+});
 
-  const db = getFirestore();
+const db = getFirestore();
 
-  const dbRT = getDatabase();
+const dbRT = getDatabase();
 
-
-export {db ,dbRT} ;
+export { db, dbRT };
